3c8e122f06ea27166a9e0bb12308cd9c849db212
Mark <markrichey@users.noreply.github.com>
2025-02-04 16:51:41 +0000

    Create fn_String_GetFullNameInitials.sql

---
+++ fn_String_GetFullNameInitials.sql
@@ -0,0 +1,63 @@
+ALTER FUNCTION dbo.fn_String_GetFullNameInitials
+
+(
+
+    @Name varchar(1000)
+
+)
+
+RETURNS varchar(100)
+
+AS
+
+BEGIN
+
+ 
+
+                DECLARE @Pos AS INT
+
+                DECLARE @inits AS VARCHAR(100) = ''
+
+                DECLARE @InitCount INT = LEN(@Name)-LEN(REPLACE(@Name,' ',''))
+
+                DECLARE @Loop AS INT = 1
+
+               
+
+                IF @InitCount >= 1
+
+                BEGIN
+
+                                WHILE @Loop <= @InitCount
+
+                                BEGIN
+
+                                                SET @Pos = CHARINDEX(' ',@Name,0)
+
+                                                IF @Pos > 0
+
+                                                BEGIN
+
+                                                                SET @inits = @inits + LEFT(LEFT(@Name,@Pos),1)
+
+                                                                SET @Name = RIGHT(@Name,LEN(@Name)-@Pos)
+
+                                                END
+
+                                                SET @Loop += 1
+
+                                END
+
+                END
+
+ 
+
+                SET @inits = @inits + left(@Name,1)
+
+ 
+
+    RETURN @inits
+
+END
+
+GO
