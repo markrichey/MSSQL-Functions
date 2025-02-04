e6165e9efca83e17ece440f4467b66464519e795
Mark <markrichey@users.noreply.github.com>
2025-02-04 16:53:29 +0000

    Create fn_String_GetInitials.sql

---
+++ fn_String_GetInitials.sql
@@ -0,0 +1,19 @@
+CREATE FUNCTION dbo.fn_String_GetInitials (
+  @Name varchar(1000)
+) RETURNS varchar(100) AS BEGIN DECLARE @Pos AS INT DECLARE @inits AS VARCHAR(100) = '' DECLARE @InitCount INT = LEN(@Name)- LEN(
+  REPLACE(@Name, ' ', '')
+) DECLARE @Loop AS INT = 1 IF @InitCount >= 1 BEGIN WHILE @Loop <= @InitCount BEGIN 
+SET 
+  @Pos = CHARINDEX(' ', @Name, 0) IF @Pos > 0 BEGIN 
+SET 
+  @inits = @inits + LEFT(
+    LEFT(@Name, @Pos), 
+    1
+  ) 
+SET 
+  @Name = RIGHT(
+    @Name, 
+    LEN(@Name)- @Pos
+  ) END 
+SET 
+  @Loop += 1 END END RETURN @inits END GO
